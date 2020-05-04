fada15e95f3b8e80ecd70f601a34c3321ab79363
root <root@ip-172-31-37-232.ec2.internal>
2020-05-04 10:31:59 +0000

    First proj

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -vvv -i pem_cd_war_vm ubuntu@ec2-54-235-6-9.compute-1.amazonaws.com -t "cd /home/ubuntu/git-projects/scripts-repository && ./sonar.sh https://github.com/govind-neova/puppeteer.git govind-test"
+ssh -vvv -i pem_key_name ubuntu@ec2-54-235-6-9.compute-1.amazonaws.com -t "cd /home/ubuntu/git-projects/scripts-repository && ./sonar.sh https://github.com/govind-neova/puppeteer.git govind-test"
